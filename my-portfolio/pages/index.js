e1781354eeeec136dec07e09544f6cc44bd62353
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2022-09-27 18:59:39 +0200

    wow

---
+++ my-portfolio/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import React from 'react'
+import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tom Thierry</title>
+      </Head>
+    </>
+  )
+}
